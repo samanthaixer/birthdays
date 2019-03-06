c1311407a82c9178c9b5e6e6a2c5d3c38d2d162d
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-03-06 12:24:46 +0000

    commit first two tests - storing birthdays and displaying birthdays

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,19 @@
 
+class BirthdayList
+  def initialize
+    @birthday_list = []
+  end
+
+  def store_birthday(name, birthday)
+    @birthday_list << {name: name, birthday: birthday}
+    return "Birthday stored!"
+  end
+
+  def display_birthdays
+    birthday_string = ""
+    @birthday_list.each do |birthday|
+        birthday_string << "Name: #{birthday[:name]} Birthday: #{birthday[:birthday]}\n "
+    end
+    return birthday_string
+  end
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,21 @@
+require 'birthday_list'
+# store_birthday(name, date)
+# display_birthdays
+# birthday?(name)
+# calculate_age
 
+describe BirthdayList do
+    it { is_expected.to respond_to(:store_birthday) }
+
+    it 'should return "birthday stored" when store_birthday is called' do
+      expect(BirthdayList.new.store_birthday("Sam", "19/03/1982")).to eq "Birthday stored!"
+    end
+
+    it 'should print a list of names and birthdays when display_birthdays is called' do
+      birthday_list = BirthdayList.new
+      birthday_list.store_birthday("Sam", "19/03/1982")
+      expect(birthday_list.display_birthdays).to eq "Name: Sam Birthday: 19/03/1982\n "
+      birthday_list.store_birthday("Riya", "14/7/1995")
+      expect(birthday_list.display_birthdays).to eq "Name: Sam Birthday: 19/03/1982\n Name: Riya Birthday: 14/7/1995\n "
+    end
+end
