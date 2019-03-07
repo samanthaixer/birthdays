a2ac8c5c6ac6fdc364255771a98f7c50dc1f813c
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-03-07 09:40:38 +0000

    updated birthday list to check for birthdays today

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,3 +1,4 @@
+require 'Date'
 
 class BirthdayList
   def initialize
@@ -15,5 +16,16 @@ class BirthdayList
         birthday_string << "Name: #{birthday[:name]} Birthday: #{birthday[:birthday]}\n "
     end
     return birthday_string
+  end
+
+  def birthday_today?
+    birthdays_today = ""
+    @birthday_list.each do | birthday |
+      birthday_date = Date.parse birthday[:birthday]
+      if birthday_date.month == Date.today.month && birthday_date.day == Date.today.day
+        birthdays_today << "It's #{birthday[:name]}'s birthday today! They are #{Date.today.year - birthday_date.year} years old!"
+      end
+    end
+    return birthdays_today
   end
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -18,4 +18,12 @@ describe BirthdayList do
       birthday_list.store_birthday("Riya", "14/7/1995")
       expect(birthday_list.display_birthdays).to eq "Name: Sam Birthday: 19/03/1982\n Name: Riya Birthday: 14/7/1995\n "
     end
+
+    it { is_expected.to respond_to(:birthday_today?) }
+
+    it 'should return "It\'s Mary\'s birthday today! They are 33 years old!"' do
+      birthday_list = BirthdayList.new
+      birthday_list.store_birthday("Mary", "07/03/1986")
+      expect(birthday_list.birthday_today?).to eq "It's Mary's birthday today! They are 33 years old!"
+    end
 end
