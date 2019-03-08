51cb46764abea5123f915422357d8afd9a5a31e4
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-03-08 11:41:59 +0000

    refactored code and tests. First 3 requirements are working

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -5,25 +5,26 @@ class BirthdayList
     @birthday_list = []
   end
 
-  def store_birthday(name, birthday)
-    @birthday_list << {name: name, birthday: birthday}
+  def store(name, birthday = Date.today)
+    @birthday_list << { name: name, birthday: birthday }
     return "Birthday stored!"
   end
 
   def display_birthdays
     birthday_string = ""
     @birthday_list.each do |birthday|
-        birthday_string << "Name: #{birthday[:name]} Birthday: #{birthday[:birthday]}\n "
+      birthday_string << "Name: #{birthday[:name]} Birthday: #{birthday[:birthday]}\n "
     end
     return birthday_string
   end
 
-  def birthday_today?
+  def todays_birthdays
     birthdays_today = ""
-    @birthday_list.each do | birthday |
-      birthday_date = Date.parse birthday[:birthday]
-      if birthday_date.month == Date.today.month && birthday_date.day == Date.today.day
-        birthdays_today << "It's #{birthday[:name]}'s birthday today! They are #{Date.today.year - birthday_date.year} years old!"
+    @birthday_list.each do |birthday|
+      birth_date = Date.parse birthday[:birthday]
+      today = Date.today
+      if birth_date.month == today.month && birth_date.day == today.day
+        birthdays_today << "It's #{birthday[:name]}'s birthday today! They are #{today.year - birth_date.year} years old!"
       end
     end
     return birthdays_today
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -5,25 +5,25 @@ require 'birthday_list'
 # calculate_age
 
 describe BirthdayList do
-    it { is_expected.to respond_to(:store_birthday) }
+    it { is_expected.to respond_to(:store) }
 
     it 'should return "birthday stored" when store_birthday is called' do
-      expect(BirthdayList.new.store_birthday("Sam", "19/03/1982")).to eq "Birthday stored!"
+      expect(BirthdayList.new.store("Sam", "19/03/1982")).to eq "Birthday stored!"
     end
 
     it 'should print a list of names and birthdays when display_birthdays is called' do
       birthday_list = BirthdayList.new
-      birthday_list.store_birthday("Sam", "19/03/1982")
+      birthday_list.store("Sam", "19/03/1982")
       expect(birthday_list.display_birthdays).to eq "Name: Sam Birthday: 19/03/1982\n "
-      birthday_list.store_birthday("Riya", "14/7/1995")
+      birthday_list.store("Riya", "14/7/1995")
       expect(birthday_list.display_birthdays).to eq "Name: Sam Birthday: 19/03/1982\n Name: Riya Birthday: 14/7/1995\n "
     end
 
-    it { is_expected.to respond_to(:birthday_today?) }
+    it { is_expected.to respond_to(:todays_birthdays) }
 
     it 'should return "It\'s Mary\'s birthday today! They are 33 years old!"' do
       birthday_list = BirthdayList.new
-      birthday_list.store_birthday("Mary", "07/03/1986")
-      expect(birthday_list.birthday_today?).to eq "It's Mary's birthday today! They are 33 years old!"
+      birthday_list.store("Mary", "08/03/1986")
+      expect(birthday_list.todays_birthdays).to eq "It's Mary's birthday today! They are 33 years old!"
     end
 end
